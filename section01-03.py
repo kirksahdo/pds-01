632ebe21c84b270988d29a2b67fced1be8d4bc12
Danilo Bruno <danilobsilv@gmail.com>
2024-06-02 21:14:06 -0400

    feature: section 01 actv 03 done

--- section01-03.py
+++ section01-03.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def plot_linearidade():
-    
+
     # Sinal x[n]
     x = np.array([0, 0.707, 1, 0.707, 0, -0.707, -1, -0.707])
     # Constante
